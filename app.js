02f762c3a8e55cb273adc3de7735d08c4b90d5c1
ASelcukTuncer <aselcuktuncer@yahoo.com>
2021-11-26 02:26:42 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-const photo = {
-  id: 11,
-  url: 'http://foo.com',
-  desc: 'Such a lovely photo!',
-};
+//Template Engine
+app.set('view engine', 'ejs');
 
+//Middlaware
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => {
-  res.send(photo);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`App is running on port number ${port}`);
+  console.log(`App is running on port number: ${port}`);
 });
